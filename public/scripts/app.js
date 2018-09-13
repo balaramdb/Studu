cbfcf1259313d60f9653a0a2878faf7afd94cd85
balaramdb <balaramdb@gmail.com>
2018-09-13 11:38:47 -0700

    register service worker

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,8 @@
-//app.js
+(function() {
+  // service worker
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+             .register('./service-worker.js')
+             .then(function() { console.log('Service Worker Registered'); });
+  }
+})();
