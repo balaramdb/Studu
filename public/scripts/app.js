6b67c7aa65d571c76807003d1f82917b9cb659d1
balaramdb <balaramdb@gmail.com>
2018-09-14 23:41:22 -0700

    tried to add a2hs dialog/button

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,39 +5,51 @@
   * Add to homescreen popup
   ********************************************************/
 
-  let deferredPrompt;
+  var deferredPrompt;
+  var a2hsBtn = document.getElementById("btnAdd");
 
-  window.addEventListener('beforeinstallprompt', (e) => {
+  window.addEventListener('beforeinstallprompt', function (e) {
+    console.log('beforeinstallprompt fired.')
     // Prevent Chrome 67 and earlier from automatically showing the prompt
     e.preventDefault();
     // Stash the event so it can be triggered later.
     deferredPrompt = e;
-    // Update UI notify the user they can add to home screen
-    btnAdd.style.display = 'block';
+
+    showAddToHomeScreen();
+    //deferredPrompt.prompt()
   });
 
-  btnAdd.addEventListener('click', (e) => {
+  function showAddToHomeScreen() {
+    a2hsBtn.style.display = "block";
+    a2hsBtn.addEventListener("click", addToHomeScreen);
+  }
+
+  function addToHomeScreen() {
     // hide our user interface that shows our A2HS button
-    btnAdd.style.display = 'none';
-    // Show the prompt
-    deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice
-      .then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the A2HS prompt');
-        } else {
-          console.log('User dismissed the A2HS prompt');
-        }
-        deferredPrompt = null;
-      });
-  });
+    a2hsBtn.style.display = 'none';
 
-  // check if app is installed
-  window.addEventListener('appinstalled', (evt) => {
-    app.logEvent('a2hs', 'installed');
-  });
+    if (deferredPrompt) {
+      // Show the prompt
+      deferredPrompt.prompt();
 
+      // Wait for the user to respond to the prompt
+      deferredPrompt.userChoice
+        .then(function (choiceResult) {
+          if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted the A2HS prompt');
+          } else {
+            console.log('User dismissed the A2HS prompt');
+          }
+          deferredPrompt = null;
+        });
+    }
+  }
+
+  /*showAddToHomeScreen();
+
+  window.addEventListener('appinstalled', function (evt) {
+    console.log('a2hs', 'installed');
+  });*/
 
 
   // service worker
